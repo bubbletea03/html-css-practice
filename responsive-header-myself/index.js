8eb95b2e02d4e308d2060dbfdc14c1cc21c7b66e
bubbletea03 <bubbletea03@naver.com>
2023-01-18 11:48:01 +0900

    모바일 버전도 직접 만들어봄

--- responsive-header-myself/index.js
+++ responsive-header-myself/index.js
@@ -1,1 +1,33 @@
-console.log("ㅎㅇ")
+const menuBtn = document.querySelector(".menu");
+
+menuBtn.addEventListener("click", () => {
+    const nav = document.querySelector("nav");
+    const itemContainer = document.querySelector(".item-container");
+    const links = document.querySelector(".links");
+
+    nav.insertAdjacentElement("afterend", itemContainer);
+
+    ITEM_CONTAINER_STYLE = "display: flex;"
+        + "flex-direction: column;"
+        + "background-color: gray;"
+        + "text-align: center;"
+        + "width: 100%;"
+        + "color: white;"
+        + "font-weight: bold;"
+        + "justify-content: space-around;"
+        + "height: 150px;";
+    itemContainer.style.cssText = ITEM_CONTAINER_STYLE;
+
+    itemContainer.insertAdjacentElement("afterend", links);
+
+    LINKS_STYLE = "display: flex;"
+    + "flex-direction: column;"
+    + "background-color: gray;"
+    + "text-align: center;"
+    + "width: 100%;"
+    + "color: white;"
+    + "font-weight: bold;"
+    + "padding: 20px 0px 20px 0px";
+
+    links.style.cssText = LINKS_STYLE;
+});
